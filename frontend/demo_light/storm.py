2f77a69deebc3ffdb2345e3d9a5ac79331191a3d
DulayelNotaifi <dulayelan09@gmail.com>
2024-11-02 11:41:50 +0300

    Update storm.py

--- frontend/demo_light/storm.py
+++ frontend/demo_light/storm.py
@@ -16,6 +16,18 @@ from streamlit_option_menu import option_menu
 def main():
     global database
     st.set_page_config(layout='wide')
+    st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
     if "first_run" not in st.session_state:
         st.session_state['first_run'] = True
