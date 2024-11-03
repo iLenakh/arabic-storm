6a18fe14920525c7aafce3b6a55e27094abe6f36
DulayelNotaifi <dulayelan09@gmail.com>
2024-11-04 02:39:26 +0300

    Update storm.py

--- frontend/demo_light/storm.py
+++ frontend/demo_light/storm.py
@@ -15,6 +15,17 @@ def main_app():
     global database
     st.set_page_config(layout='wide')
 
+        # Display user info if logged in
+    if "user_info" in st.session_state:
+        user_info = st.session_state["user_info"]
+        user_name = user_info.get("name", "User")
+        user_email = user_info.get("email", "No email available")
+        
+        st.sidebar.write(f"**Logged in as:** {user_name}")
+        st.sidebar.write(f"**Email:** {user_email}")
+    else:
+        st.error("User information is missing. Please log in again.")
+
     # First-time setup
     if "first_run" not in st.session_state:
         st.session_state['first_run'] = True
