2d928c02b4605ac7123ddaf8514882362188c5ed
DulayelNotaifi <dulayelan09@gmail.com>
2024-11-04 02:29:00 +0300

    Update Landing_page.py

--- frontend/demo_light/pages_util/Landing_page.py
+++ frontend/demo_light/pages_util/Landing_page.py
@@ -25,8 +25,8 @@ def google_login():
 
 # Function to handle Google OAuth response
 def handle_google_callback():
-    # Retrieve authorization code from query parameters
-    code = st.query_params.get("code")
+    # Retrieve authorization code from query parameters 
+    code = st.query_params("code")
     if not code:
         st.error("Authorization code not found. Please log in again.")
         return
